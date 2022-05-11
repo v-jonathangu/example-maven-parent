0025b26116f7de1a6ccf24b1b8a72436adc46909
Jonathan Guzmán <jonguz5@hotmail.com>
2022-05-11 11:13:08 -0600

    init repo

---
+++ src/main/java/com/example/demoparent/DemoParentApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoParentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoparent/DemoParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
